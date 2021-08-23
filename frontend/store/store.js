fab2a4e1f51ac6999c2f207f55090f9b59a9342a
Alec Caro <alec.caro@gmail.com>
2021-08-23 16:28:38 -0400

    removed logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -4,7 +4,7 @@ import logger from 'redux-logger';
 import rootReducer from '../reducers/root_reducer';
 
 const configureStore = (preloadedState = {}) => (
-  createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger))
+  createStore(rootReducer, preloadedState, applyMiddleware(thunk)) //removed "logger" as 2nd arg in applyMiddleware
 );
 
 export default configureStore;
